0f5f2334cbec7d2199cbe1c3cc9dd348baeefae9
codeconcept <contact@codeconcept.fr>
2019-03-06 19:37:48 +0100

    add food form

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+  form: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      foodName: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      datePlacedInFreezer: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
+  }
+
+  add() {
+    console.log(this.form);
+  }
+}
