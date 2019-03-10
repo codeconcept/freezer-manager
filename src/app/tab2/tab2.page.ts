7abb59d9702c3e17548460e40ec8c83ca4aae7f6
codeconcept <contact@codeconcept.fr>
2019-03-10 13:45:25 +0100

    alert that asks for deletion confirmation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,7 +3,7 @@ import { FoodService } from './../shared/food.service';
 import { Food } from '../shared/food.model';
 import { Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 import { FoodEditPage } from '../food/food-edit/food-edit.page';
 
 @Component({
@@ -16,7 +16,7 @@ export class Tab2Page implements OnInit, OnDestroy {
   sub: Subscription;
   isLoading = false;
 
-  constructor(private foodService: FoodService, private modalCtrl: ModalController) {}
+  constructor(private foodService: FoodService, private modalCtrl: ModalController, private alertCtrl: AlertController) {}
 
   ngOnInit() {
     this.sub = this.foodService.allFood().subscribe(data => {
@@ -41,16 +41,38 @@ export class Tab2Page implements OnInit, OnDestroy {
   delete(id) {
     console.log('id', id);
     this.isLoading = true;
-    this.foodService
-      .deleteFood(id)
-      .pipe(
-        take(1)
-      )
-      .subscribe(() => {
-        this.isLoading = false;
-      }, err => {
-        console.error(err);
-      });
+    this.alertCtrl.create({
+      header: 'Delete this food?',
+      subHeader: 'deletion is irreversible',
+      buttons: [
+        {
+          text: 'Cancel',
+          cssClass: 'primary',
+          role: 'cancel',
+          handler: () => {
+            this.isLoading = false;
+          }
+        },
+        {
+          text: 'delete',
+          cssClass: 'danger',
+          handler: () => {
+            this.foodService
+              .deleteFood(id)
+              .pipe(
+                take(1)
+              )
+              .subscribe(() => {
+                this.isLoading = false;
+              }, err => {
+                console.error(err);
+              });
+          }
+        }
+      ]
+    }).then(alert => {
+      alert.present();
+    });
 
   }
 
